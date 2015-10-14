a6aab14ec2fbdbb8024b86ee145d3aa7782a1183
dasheq <dasheq@hotmail.com>
2015-10-14 18:29:09 +0300

    Nightmare

---
+++ lab5/my_application/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return "Index Page"
+@app.route("/hello")
+def hello():
+	return "Hello World!"
+@app.route("/user/paul")
+def user():
+	return "User paul"
+@app.route("/post/80")
+def post():
+	return "Post 80"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
